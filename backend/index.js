a05dd0c3cc75908872f9ecfb6f07e9c8da12b4b3
Samiksha Jain <samikshaj7371@gmail.com>
2026-06-20 18:10:35 +0530

    create authentication middleware

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
 import connectDb from "./config/db.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
-import cors from "cors"
+import cors from "cors";
+import userRouter from "./routes/user.routes.js";
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/auth",authRouter)
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.listen(port,()=>{
-    connectDb()
-    console.log(`server is started at the ${port}`)
-})
+app.listen(port, () => {
+  connectDb();
+  console.log(`server is started at the ${port}`);
+});

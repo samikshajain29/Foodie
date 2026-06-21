21412c584ff3b64c77b8bf7fe27c2469acfb854f
Samiksha Jain <samikshaj7371@gmail.com>
2026-06-21 15:31:29 +0530

    set up redux toolkit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 const serverUrl = "http://localhost:8000";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 );

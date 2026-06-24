6a370f43d6f612da65ddfeb67cdd81d51d36574b
Samiksha Jain <samikshaj7371@gmail.com>
2026-06-24 20:13:28 +0530

    Make responsive

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,12 +3,30 @@ import { FaLocationDot } from "react-icons/fa6";
 import { IoIosSearch } from "react-icons/io";
 import { FiShoppingCart } from "react-icons/fi";
 import { useSelector } from "react-redux";
+import { RxCross2 } from "react-icons/rx";
 
 function Nav() {
   const { userData } = useSelector((state) => state.user);
   const [showInfo, setShowInfo] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="w-full h-20 flex items-center justify-between md:justify-center gap-7.5 px-5 fixed top-0 z-9999 bg-[#fff9f6] overflow-visible">
+      {showSearch && (
+        <div className="w-[90%] h-17.5 bg-white shadow-xl rounded-lg items-center gap-5 flex fixed top-20 left-[5%]">
+          <div className="flex items-center w-[30%] overflow-hidden gap-2.5 px-2.5 border-r-2 border-gray-400">
+            <FaLocationDot size={25} className="text-[#ff4d2d]" />
+            <div className="w-[80%] truncate text-gray-600">Arrah</div>
+          </div>
+          <div className="w-[80%] flex items-center gap-2.5">
+            <IoIosSearch size={25} className="text-[#ff4d2d]" />
+            <input
+              type="text"
+              placeholder="search delicious food..."
+              className="px-2.5 text-gray-700 outline-0 w-full"
+            />
+          </div>
+        </div>
+      )}
       <h1 className="text-3xl font-bold mb-2 text-[#ff4d2d]">Foodie</h1>
       <div className="md:w-[60%] lg:w-[40%] h-17.5 bg-white shadow-xl rounded-lg items-center gap-5 hidden md:flex">
         <div className="flex items-center w-[30%] overflow-hidden gap-2.5 px-2.5 border-r-2 border-gray-400">
@@ -25,7 +43,19 @@ function Nav() {
         </div>
       </div>
       <div className="flex items-center gap-4">
-        <IoIosSearch size={25} className="text-[#ff4d2d] md:hidden" />
+        {showSearch ? (
+          <RxCross2
+            size={25}
+            className="text-[#ff4d2d] md:hidden"
+            onClick={() => setShowSearch(false)}
+          />
+        ) : (
+          <IoIosSearch
+            size={25}
+            className="text-[#ff4d2d] md:hidden"
+            onClick={() => setShowSearch(true)}
+          />
+        )}
         <div className="relative cursor-pointer">
           <FiShoppingCart size={25} className="text-[#ff4d2d]" />
           <span className="absolute -right-2.25 -top-3 text-[#ff4d2d]">0</span>

c9a0945a3923b9a2401fab3f525526e3b27e2521
Samiksha Jain <samikshaj7371@gmail.com>
2026-06-24 21:21:59 +0530

    complete nav component

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -2,20 +2,34 @@ import React, { useState } from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoIosSearch } from "react-icons/io";
 import { FiShoppingCart } from "react-icons/fi";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RxCross2 } from "react-icons/rx";
+import axios from "axios";
+import { serverUrl } from "../App";
+import { setUserData } from "../redux/userSlice";
 
 function Nav() {
-  const { userData } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const { userData, city } = useSelector((state) => state.user);
   const [showInfo, setShowInfo] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
+  const handleLogOut = async () => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/auth/signout`, {
+        withCredentials: true,
+      });
+      dispatch(setUserData(null));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="w-full h-20 flex items-center justify-between md:justify-center gap-7.5 px-5 fixed top-0 z-9999 bg-[#fff9f6] overflow-visible">
       {showSearch && (
-        <div className="w-[90%] h-17.5 bg-white shadow-xl rounded-lg items-center gap-5 flex fixed top-20 left-[5%]">
+        <div className="w-[90%] h-17.5 bg-white shadow-xl rounded-lg items-center gap-5 flex fixed top-20 left-[5%] md:hidden">
           <div className="flex items-center w-[30%] overflow-hidden gap-2.5 px-2.5 border-r-2 border-gray-400">
             <FaLocationDot size={25} className="text-[#ff4d2d]" />
-            <div className="w-[80%] truncate text-gray-600">Arrah</div>
+            <div className="w-[80%] truncate text-gray-600">{city}</div>
           </div>
           <div className="w-[80%] flex items-center gap-2.5">
             <IoIosSearch size={25} className="text-[#ff4d2d]" />
@@ -31,7 +45,7 @@ function Nav() {
       <div className="md:w-[60%] lg:w-[40%] h-17.5 bg-white shadow-xl rounded-lg items-center gap-5 hidden md:flex">
         <div className="flex items-center w-[30%] overflow-hidden gap-2.5 px-2.5 border-r-2 border-gray-400">
           <FaLocationDot size={25} className="text-[#ff4d2d]" />
-          <div className="w-[80%] truncate text-gray-600">Arrah</div>
+          <div className="w-[80%] truncate text-gray-600">{city}</div>
         </div>
         <div className="w-[80%] flex items-center gap-2.5">
           <IoIosSearch size={25} className="text-[#ff4d2d]" />
@@ -75,7 +89,10 @@ function Nav() {
             <div className="md:hidden text-[#ff4d2d] font-semibold cursor-pointer">
               My Orders
             </div>
-            <div className="text-[#ff4d2d] font-semibold cursor-pointer">
+            <div
+              className="text-[#ff4d2d] font-semibold cursor-pointer"
+              onClick={handleLogOut}
+            >
               Log Out
             </div>
           </div>
---
+++ frontend/src/hooks/useGetCity.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setCity } from "../redux/userSlice";
+
+function useGetCity() {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+  const apikey = import.meta.env.VITE_GEOAPIKEY;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const result = await axios.get(
+        `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${apikey}`,
+      );
+      dispatch(setCity(result?.data?.results[0].city));
+    });
+  }, [userData]);
+}
+
+export default useGetCity;

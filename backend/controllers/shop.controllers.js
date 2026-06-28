0074780b340415adbdb1961db521338073c76144
Samiksha Jain <samikshaj7371@gmail.com>
2026-06-28 21:51:50 +0530

    get current shop

--- backend/controllers/shop.controllers.js
+++ backend/controllers/shop.controllers.js
@@ -39,3 +39,17 @@ export const createEditShop = async (req, res) => {
     return res.status(500).json({ message: `create shop error ${error}` });
   }
 };
+export const getMyShop = async (req, res) => {
+  try {
+    const shop = await Shop.findOne({ owner: req.userId }).populate(
+      "owner",
+      "items",
+    );
+    if (!shop) {
+      return null;
+    }
+    return res.status(200).json(shop);
+  } catch (error) {
+    return res.status(500).json({ message: `get my shop error ${error}` });
+  }
+};
--- backend/routes/shop.routes.js
+++ backend/routes/shop.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import { createEditShop } from "../controllers/shop.controllers.js";
+import { createEditShop, getMyShop } from "../controllers/shop.controllers.js";
 import { upload } from "../middlewares/multer.js";
 const shopRouter = express.Router();
 
 shopRouter.post("/create-edit", isAuth, upload.single("image"), createEditShop);
+shopRouter.get("/get-my", isAuth, getMyShop);
 
 export default shopRouter;

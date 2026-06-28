0b903fba2d676e874ef0d29a62c81a2260f2ff68
Samiksha Jain <samikshaj7371@gmail.com>
2026-06-28 20:19:19 +0530

    Create item controller and routes

--- backend/routes/shop.routes.js
+++ backend/routes/shop.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
 import { createEditShop } from "../controllers/shop.controllers.js";
+import { upload } from "../middlewares/multer.js";
 const shopRouter = express.Router();
 
-shopRouter.post("/create-edit", isAuth, createEditShop);
+shopRouter.post("/create-edit", isAuth, upload.single("image"), createEditShop);
 
 export default shopRouter;
